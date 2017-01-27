0fefc984d74618b5627843148a017d0d4764f659
Nikhil Saldanha <nikhil.saldanha691@gmail.com>
2017-01-28 00:46:07 +0530

    Tokenizer can now recognize relational operators and bitwise operators. Tokenizer now correctly tokenizes strings as a whole. Slight restructuring of code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
 use std::str::Chars;
 
-struct Tokenizer<'a>{
+struct Tokenizer<'a> {
 	pos: usize,
 	current_char:char,
 	token:Vec<char>,
 	length:usize,
 	input:Chars<'a>,
 	token_buffer:Vec<String>,
-	}
+}
 
-impl<'a> Tokenizer<'a>{
+impl<'a> Tokenizer<'a> {
 
 	/* tokenizer constructor
 	 * Create object of type Tokenizer
 	 * and returns it
 	 */
-	fn new(text:&str)-> Tokenizer{
+	fn new(text:&str)-> Tokenizer {
 		
 		let token:Vec<char> = Vec::new();
 		let token_stream:Vec<String> = Vec::new();
-		//create structure object and initialize
-		let self_object = Tokenizer{ pos:0,current_char:' ',
-				   length:text.len(),
-				   token:token,
-				   token_buffer:token_stream,
-				   input:text.chars()
-				   };
-		self_object
+
+		// create structure object and initialize
+		let self_object = Tokenizer {
+            pos:0,
+            current_char:' ',
+            length:text.len(),
+            token:token,
+            token_buffer:token_stream,
+            input:text.chars()
+        };
+        self_object
 	}
 
 	/* tokenize
@@ -34,87 +37,110 @@ impl<'a> Tokenizer<'a>{
 	 * returns the stream of tokens
 	 */
 	fn tokenize(&mut self)-> Vec<String>{
-
-		//token vector to be returned
-	//	let mut token_buffer:Vec<String> =Vec::new();
-
+        // token vector to be returned
+	    // let mut token_buffer:Vec<String> =Vec::new();
+        
+        self.current_char = self.get_next_char();
 		loop{	
 			//get the character at self.pos
-			if self.pos<self.length-1 {
-							self.current_char = self.get_next_char();
-						} else {
-							//make sure everything is dumped
-							break;
-						}
+            //} else {
+                //make sure everything is dumped
+             //   break;
+           // }
 			
 			//println!(" char {} ",self.current_char);
 			
-			match self.current_char{
-					     ' '|'\n'|'\t' =>{
-						 self.push_to_tok_buffer();
-						 	continue;	
-						 },
+            match self.current_char {
+                ' '|'\n'|'\t' => {
+                    self.push_to_tok_buffer();
+                    self.current_char = self.get_next_char();
+                },
+                t @ '"'=> {
+                    self.token.push(t);
+//                    self.push_to_tok_buffer();
+                    let mut ch = self.get_next_char();
+                    while ch!='"' {
+                        self.token.push(ch);
+                        ch = self.get_next_char();
+                    }
+                    self.token.push('\"');
+					self.push_to_tok_buffer();
 
-						
-						 t @ '"'=> {
-						 	self.token.push(t);
-							self.push_to_tok_buffer();
-							let mut ch = self.get_next_char();
-						 	while ch!='"' {
-									self.token.push(ch);
-									ch = self.get_next_char();
-								}
-							
-							self.push_to_tok_buffer();
-							self.token_buffer.push("\"".to_string());
+                    self.current_char = self.get_next_char();
+                },
+                t @ '{'| t @ '('| t @ '[' | t @ '}'| t @ ')'| t @ ']'=> {
+                    self.token.push(t);
+                    self.push_to_tok_buffer();
+                    self.current_char = self.get_next_char()
+				},
+                t @ '<' | t @ '>' | t @ '=' => {
+                    self.token.push(t);
+                    let ch = self.get_next_char();
+                    match ch {
+                        '>' | '<' | '=' => {
+                            self.token.push(ch);
+                            self.current_char = self.get_next_char()
+                        }
+                        _ => {}
+                    }
+                    self.push_to_tok_buffer();
+                }
+                t @ 'a' ... 'z' | t @ 'A' ... 'Z' => {
+                    self.token.push(t);
+                    while true {
+                        let ch = self.get_next_char();
+                        match ch {
+                            'a' ... 'z' | 'A' ... 'Z' | '0' ... '9' => {
+                                self.token.push(ch);
+                            }
+                            _ => {
+                                self.current_char = ch;
+                                break;
+                            }
+                        }
+                    }
+                    self.push_to_tok_buffer();
+                }
+				e @  _ => {
 
-						 	
-						 },
-
-						 t @ '{'| t @ '('| t @ '[' | t @ '}'| t @ ')'| t @ ']'=>{
-							
-							
-						 	self.push_to_tok_buffer();
-							//push the char into current token set
-							self.token.push(t);
-
-
-							self.push_to_tok_buffer();
-							
-						},
-						e @  _ => {
-							self.token.push(e);		
-						},
-				}
-			
+                    self.token.push(e);		
+                    self.push_to_tok_buffer();
+                    self.current_char = self.get_next_char();
+				},
+			}
+			if self.pos > self.length {
+                break;
+            }
 		}
-		
-		//return the stream clone to struct internal object
+        
+        //return the stream clone to struct internal object
 		self.token_buffer.clone()	
 	}
 
-	
 	/* get_next_token:
 	 * returns the next char in a input stream
 	 * pointed by `pos` position
 	 */
 	 fn get_next_char(&mut self)->char {
-		let ch=self.input.next().unwrap();
 		self.pos+=1;
-		ch
+		if let Some(ch)=self.input.next() {
+            ch
+        } else {
+            ' '
+        }
 	}
 	
 
 	/* function to put each token into
 	 * the token stream as it read
 	 */
-	fn push_to_tok_buffer(&mut self){
+	fn push_to_tok_buffer(&mut self) {
 		let token:String = self.token.iter().cloned().collect();
-		if !token.is_empty(){
+		if !token.is_empty() {
 			self.token_buffer.push(token);
-			}
-		self.token.clear();
 		}
+		self.token.clear();
+	}
 
 }
 
@@ -122,21 +148,21 @@ impl<'a> Tokenizer<'a>{
 
 
 fn main() {
-   // let mut tok = Tokenizer{pos:-1,current_char:' ',token:&Vec<char>::new()};
-   let text = "int main(){
-    			cout<<\"hello world\";
-			}".to_string();
-    
-   let mut tok = Tokenizer::new(&text);
+    // let mut tok = Tokenizer{pos:-1,current_char:' ',token:&Vec<char>::new()};
+
+    let text = "int main(){
+                    cout<<\"hello world\";
+                }".to_string();
+    let mut tok = Tokenizer::new(&text);
     println!("Tokenizing: {}",text);
-  /*  for i in 0..text.len(){
-    	println!(" {} ",text.chars().nth(i).unwrap());
+    /*  for i in 0..text.len(){
+        println!(" {} ",text.chars().nth(i).unwrap());
 	}
     */
     
     let tokens = tok.tokenize();
 
-    for i in tokens{
-    		println!["<< {} >>",i];
-		}
+    for i in tokens {
+        println!["<< {} >>",i];
+	}
 }
