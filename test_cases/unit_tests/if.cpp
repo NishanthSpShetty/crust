c79b063fdc21958b9c8abd1ceb151f5c15819db1
NishanthSpShetty <nishanthspshetty@gmail.com>
2017-02-18 16:08:37 +0530

    Check this program input , compile output.rs with rustc . Try removing semicolon after comment in if and check the output

--- test_cases/unit_tests/if.cpp
+++ test_cases/unit_tests/if.cpp
@@ -1,1 +1,16 @@
-//hello world
+int a=100;
+
+int main (){
+    if(a==100){
+        //shadow a 
+      ;
+        int ab=100087;
+     another_func(ab);
+    }
+}
+
+
+int another_func(int ba){
+    println!(" Got a {} ",ba);
+    return 100;
+}
--- test_cases/unit_tests/output.rs
+++ test_cases/unit_tests/output.rs
@@ -1,1 +1,12 @@
-//hello world
+static a: i32 = 100;
+fn main() {
+    if a == 100 {
+        //shadow a
+;        let ab: i32 = 100087;
+        another_func(ab);
+    }
+}
+fn another_func(ba: i32) -> i32 {
+    println!(" Got a {} ", ba);
+    return 100;
+}
