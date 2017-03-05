0f30a382fd661f3d2835845e0d70cb53da092c50
NishanthSpShetty <nishanthspshetty@gmail.com>
2017-03-05 12:39:56 +0530

    ISSUE03: Extra closing SBRACE at the end of array declaration[FALSE]

--- src/library/parser/mod.rs
+++ src/library/parser/mod.rs
@@ -1399,7 +1399,7 @@ fn parse_array_declaration(lexeme: &Vec<Token>) -> Vec<String> {
             });
             head += 1;
         }
-       // stream.push("]".to_string());
+        stream.push("]".to_string());
         stream.push(";".to_string());
     } else {
         stream.push(";".to_string());
